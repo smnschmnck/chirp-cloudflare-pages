8d28458869ffb7af5fc257195c8569f6097bd9c9
smnschmnck <simon.schmeinck@gmx.de>
2023-12-29 21:33:04 +0100

    order posts by newest

--- functions/server/router.ts
+++ functions/server/router.ts
@@ -2,13 +2,18 @@ import { posts } from "@functions/db/schema";
 import { initTRPC } from "@trpc/server";
 import { Context } from "./context";
 import { z } from "zod";
+import { desc } from "drizzle-orm";
 
 export const t = initTRPC.context<Context>().create();
 
 export const appRouter = t.router({
   getAllPosts: t.procedure.query(async ({ ctx }) => {
     const { db } = ctx;
-    const allPosts = await db.select().from(posts).limit(100);
+    const allPosts = await db
+      .select()
+      .from(posts)
+      .orderBy(desc(posts.created_at))
+      .limit(100);
     return allPosts;
   }),
   createPost: t.procedure
