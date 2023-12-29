61c75ace43d8f162efe38ba254ec2a1a15239d49
smnschmnck <simon.schmeinck@gmx.de>
2023-12-29 21:15:28 +0100

    improve design (a little)

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -20,15 +20,30 @@ export const Home: FC = () => {
   };
 
   return (
-    <div>
-      <form onSubmit={submitPost}>
-        <input
-          value={postContent}
-          onChange={(e) => setPostContent(e.target.value)}
-        />
-        <button type="submit">Submit</button>
-      </form>
-      <ul>{posts?.map((post) => <li>{post.content}</li>)}</ul>
+    <div className="w-full flex justify-center h-full bg-gray-950">
+      <div className="flex flex-col border-x h-full text-white w-96">
+        <form
+          onSubmit={submitPost}
+          className="min-h-20 px-4 gap-4 flex items-center w-full border-b"
+        >
+          <input
+            value={postContent}
+            onChange={(e) => setPostContent(e.target.value)}
+            placeholder="Submit a post"
+            className="w-full bg-transparent h-10 px-2"
+          />
+          <button className="h-10 px-4 bg-blue-500 rounded-md">Submit</button>
+        </form>
+        <div className="h-full overflow-y-scroll">
+          <ul>
+            {posts?.map((p) => (
+              <li className="border-b h-12 flex px-4 items-center" key={p.id}>
+                {p.content}
+              </li>
+            ))}
+          </ul>
+        </div>
+      </div>
     </div>
   );
 };
