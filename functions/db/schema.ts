a5f0a6c778147d8e766f56431d5a9c2ab05556f6
smnschmnck <simon.schmeinck@gmx.de>
2023-12-30 16:35:09 +0100

    Add auth schema

--- functions/db/schema.ts
+++ functions/db/schema.ts
@@ -1,5 +1,5 @@
 import { sql } from "drizzle-orm";
-import { text, sqliteTable } from "drizzle-orm/sqlite-core";
+import { integer, sqliteTable, text } from "drizzle-orm/sqlite-core";
 
 export const posts = sqliteTable("posts", {
   id: text("id").primaryKey(),
@@ -7,4 +7,25 @@ export const posts = sqliteTable("posts", {
   created_at: text("created_at")
     .notNull()
     .default(sql`CURRENT_TIMESTAMP`),
+});
+
+export const user = sqliteTable("user", {
+  id: text("id", { length: 15 }).notNull().primaryKey(),
+});
+
+export const userKey = sqliteTable("user_key", {
+  id: text("id", { length: 255 }).notNull().primaryKey(),
+  userId: text("user_id", { length: 15 })
+    .notNull()
+    .references(() => user.id, { onDelete: "cascade" }),
+  hashedPassword: text("hashed_password", { length: 255 }),
+});
+
+export const userSession = sqliteTable("user_session", {
+  id: text("id", { length: 127 }).notNull().primaryKey(),
+  userId: text("user_id", { length: 15 })
+    .notNull()
+    .references(() => user.id, { onDelete: "cascade" }),
+  activeExpires: integer("active_expires", { mode: "number" }).notNull(),
+  idleExpires: integer("idle_expires", { mode: "number" }).notNull(),
 });
